5500c4b5b3ef5aaf36db29ddf33870fa805afb5b
t.gazzaev <t.gazzaev@sftpro.ru>
2023-03-14 21:25:56 +0300

    fix mac video

--- src/sections/Intro/index.tsx
+++ src/sections/Intro/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
 import { useTranslation } from 'react-i18next';
-import video from '../../assets/images/intro.mp4';
 
 import styles from './styles.m.scss';
 
@@ -9,12 +8,11 @@ const Intro = () => {
   const { t } = useTranslation();
   return (
     <section>
-      <div
-        className={styles.introContainer}
-        dangerouslySetInnerHTML={{
-          __html: `<video class="intro-video" autoplay muted loop playsinline preload="yes"><source src=${video} type="video/mp4" /></video>`,
-        }}
-      ></div>
+      <div className={styles.introContainer}>
+        <video className={styles.video} autoPlay muted playsInline>
+          <source src="https://dev.stackforce.ru/intro.mp4" type="video/mp4" />
+        </video>
+      </div>
       <Tilt className={styles.titleContainer} perspective={5000} gyroscope={true} tiltReverse>
         <div className={styles.title}>
           <h1 className={styles.h1Desktop}>{t('title.orgName')}</h1>
