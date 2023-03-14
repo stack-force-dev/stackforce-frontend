2fe3540887b6f9f2f6b7b294f75e1b4e6f1c0023
sergey_alekseev <salekseev@7tech.ru>
2023-03-14 19:44:12 +0300

    Intro section

--- src/sections/Intro/index.tsx
+++ src/sections/Intro/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import vid from '../../assets/images/intro.mp4';
+import Tilt from 'react-parallax-tilt';
+import { useTranslation } from 'react-i18next';
+import video from '../../assets/images/intro.mp4';
 
 import styles from './styles.m.scss';
 
 const Intro = () => {
+  const { t } = useTranslation();
   return (
     <section>
       <div className={styles.introContainer}>
         <video className={styles.video} autoPlay muted loop>
-          <source src={vid} type="video/mp4" />
+          <source src={video} type="video/mp4" />
         </video>
       </div>
+      <Tilt className={styles.titleContainer} perspective={5000} gyroscope={true} tiltReverse>
+        <div className={styles.title}>
+          <h1 className={styles.h1Desktop}>{t('title.orgName')}</h1>
+          <h1 className={styles.h1Mobile}>
+            {t('title.stack')}
+            <br />
+            {t('title.force')}
+          </h1>
+          <h3>{t('title.team')}</h3>
+        </div>
+      </Tilt>
     </section>
   );
 };
