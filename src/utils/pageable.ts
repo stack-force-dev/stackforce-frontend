9ec35694676c4a290e549f1f041a4d9fc37b217c
t.gazzaev <t.gazzaev@sftpro.ru>
2023-03-16 01:21:01 +0300

    change delay

--- src/utils/pageable.ts
+++ src/utils/pageable.ts
@@ -3,25 +3,20 @@ import Pageable from 'pageable';
 const whiteScreens = [2];
 
 export const initPageable = (setHeaderDark) => {
-  let lastScroll = 0;
   let currentScreen = 0;
 
   new Pageable('#container', {
-    anchors: ['Page 1', 'Page 2', 'Page 3'],
     animation: 600,
-    delay: 500,
+    delay: 350,
     swipeThreshold: 20,
     onFinish: (e) => {
       if (currentScreen === e.index) return;
       if (e.index > currentScreen) setHeaderDark(whiteScreens.includes(e.index));
 
-      lastScroll = e.scrolled;
       currentScreen = e.index;
     },
     onScroll: (e) => {
       if (currentScreen === e.index) return;
-
-      lastScroll = e.scrolled;
 
       if (e.index > currentScreen) return;
       setHeaderDark(whiteScreens.includes(e.index));
